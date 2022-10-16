220f57e5eee0d6a3ff00024534b9a01643a9b834
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-10-16 14:16:42 -0400

    🎉 Created IconBuddy.sh

---
+++ iconbuddy.sh
@@ -0,0 +1,11 @@
+cd ~
+mkdir -p IconBuddy
+cd ~/IconBuddy
+for i in *.icns; do
+    [ -f "$i" ] || break
+    filename="${i%.*}"
+    ICNSname=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIconFile" "/Applications/${filename}.app/Contents/Info.plist")
+    cp "$i" "/Applications/${filename}.app/Contents/Resources/${ICNSname}"
+    touch "/Applications/${filename}.app"
+    echo "✅ Changed icon for ${filename}"
+done
