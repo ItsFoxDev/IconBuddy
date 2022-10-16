409874b8cf74bd49bed35244863bd84b6c6bc201
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-10-16 14:34:12 -0400

    🚧 Always append .icns

--- iconbuddy.sh
+++ iconbuddy.sh
@@ -5,6 +5,9 @@ for i in *.icns; do
     [ -f "$i" ] || break
     filename="${i%.*}"
     ICNSname=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIconFile" "/Applications/${filename}.app/Contents/Info.plist")
+    if ! [[ "$ICNSname" == *".icns"* ]]; then
+        ICNSname="${ICNSname}.icns"
+    fi
     cp "$i" "/Applications/${filename}.app/Contents/Resources/${ICNSname}"
     touch "/Applications/${filename}.app"
     echo "✅ Changed icon for ${filename}"
